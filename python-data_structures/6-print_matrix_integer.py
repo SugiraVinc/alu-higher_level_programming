f04d464d6718038dd3d8bcb9dac00a6da58ff606
SugiraVinc <sugiravincent11@gmail.com>
2022-07-12 16:22:20 -0700

    Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for raws in matrix:
         j = 0
-        for i in rows:
+        for i in raws:
             if j == (len(raws) -1):
                 print("{d}".format(i), end="")
             else:
