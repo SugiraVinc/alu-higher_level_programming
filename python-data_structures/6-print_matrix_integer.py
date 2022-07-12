ccfa293fb55962ff38f2699e4df06553a89b3b38
SugiraVinc <sugiravincent11@gmail.com>
2022-07-12 16:20:03 -0700

    Matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for raws in matrix:
+        j = 0
+        for i in rows:
+            if j == (len(raws) -1):
+                print("{d}".format(i), end="")
+            else:
+                print("{:d}".format(i), end=" ")
+            j +=1
+        print("")
