494e5402750c5ddd57a35fc206cfc50aeb95da6b
SugiraVinc <sugiravincent11@gmail.com>
2022-07-12 09:39:57 -0700

    our print of integers without import module

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # A program that prints the list of integers or elements
-def print_list_integer(my_list[]):
+def print_list_integer(my_list[1, 2, 3]):
     for i in my_list:
         print("{:d}".format(i))
