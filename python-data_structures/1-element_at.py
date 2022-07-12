30a8c7279f0fc9743850a35e55ba93ed1801158e
SugiraVinc <sugiravincent11@gmail.com>
2022-07-12 10:23:37 -0700

    retrieve elements like in C

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#A function that retrieves elements from a list like in C
+# A function that retrieves elements from a list like in C 
 def element_at(my_list, idx):
     if idx < 0:
         return("None")
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return("None")
