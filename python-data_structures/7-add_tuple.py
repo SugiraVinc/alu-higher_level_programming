47d4c279884dc844724fb4c4ab2d2ce36700f3a1
SugiraVinc <sugiravincent11@gmail.com>
2022-07-14 07:14:17 -0700

    helps us add elements of tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -10,25 +10,25 @@ def add_tuple(tuple_a=(), tuple_b=()):
             add_a = list(tuple_a)
             add_a.append(0)
             add_a.append(0)
-        elif len_b < 1:
+        if len_b < 1:
             add_b = list(tuple_b)
             add_b.append(0)
             add_b.append(0)
-        elif len_a == 1:
+        if len_a == 1:
             add_a = list(tuple_a)
             add_a.append(0)
-        elif len_b == 1:
+        if len_b == 1:
             add_b = list(tuple_b)
             add_b.append(0)
-        elif len_a >= 2:
+        if len_a >= 2:
             add_a = list(tuple_a)
-        elif len_b >= 2:
+        if len_b >= 2:
             add_b = list(tuple_b)
         add_element.append(add_a[0] + add_b[0])
         add_element.append(add_a[1] + add_b[1])
         tuple_c = tuple(add_element)
         return tuple_c
-    else len_a >= and len_b >= 2:
+    if len_a >= and len_b >= 2:
         add_element.append(add_a[0] + add_b[0])
         add_element.append(add_a[1] + add_b[1])
         tuple_c = tuple(add_element)
