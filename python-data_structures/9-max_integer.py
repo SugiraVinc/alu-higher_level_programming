29e6af60c7ffd19d80d507fc559976ec97a3de50
SugiraVinc <sugiravincent11@gmail.com>
2022-07-14 12:12:18 -0700

    maximum

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list[]):
+    if len(my_list) <= 0:
+        return "None"
+    else:
+        maximum = my_list[]
+        for i in my_list:
+            if i > maximum:
+                maximu = i
+        return maximum
