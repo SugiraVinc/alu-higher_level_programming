d4f8bca23da8aeeee60632aa7abe9025c4fd7870
SugiraVinc <sugiravincent11@gmail.com>
2022-07-14 12:14:22 -0700

    maximum

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list[]):
+def max_integer(my_list=[]):
     if len(my_list) <= 0:
         return "None"
     else:
-        maximum = my_list[]
+        maximum = my_list[0]
         for i in my_list:
             if i > maximum:
                 maximu = i
