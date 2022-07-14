10bad0e63e13684543772982939db53bb055a9ff
SugiraVinc <sugiravincent11@gmail.com>
2022-07-14 12:01:52 -0700

    multiple returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    val =[]
+    length = len(sentence)
+    if length < 1:
+        val.append(length)
+        val.append("None")
+    else:
+        val.append(length)
+        val.append(Sentence[0])
+    last_char = tuple(val)
+    return last_char
