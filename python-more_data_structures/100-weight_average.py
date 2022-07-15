7b3411bf7056856d1a067a312b417e098ca7829d
SugiraVinc <sugiravincent11@gmail.com>
2022-07-15 13:06:30 -0700

    Weighted averaage

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+# A function that weighted average of all integers tuple
+def weight_average(my_list=[]):
+    if len(my_list) <= 0:
+        return 0
+    else:
+        total_weight = 0
+        number_of = 0
+        for temp in my_list:
+            total_weight += (temp[0] * temp[1])
+            number_of += temp[1]
+        average = total_weight/number_of
+        return average
