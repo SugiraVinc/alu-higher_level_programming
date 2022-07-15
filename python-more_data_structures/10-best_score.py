9e3a1f85c381d9db68388bb60da5e6b4071cd95e
SugiraVinc <sugiravincent11@gmail.com>
2022-07-15 12:53:37 -0700

    a keey with the biggest integer value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+# a function that return a key with the biggest integer value
+def best_score(a_):
+    if a_ is None:
+        return "None"
+    elif len(a_) > 0:
+        dict_me = dict(sorted(a_.items(), key=lambda x: x[1], reverse=True))
+        list_me = list(dict_me)
+        return list_me[0]
+    else:
+        return "None"
