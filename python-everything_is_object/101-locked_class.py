b0cc5c78516941b98692911ebb48babc6b28e594
SugiraVinc <sugiravincent11@gmail.com>
2022-07-25 14:46:07 -0700

    Using slot

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+'''
+A BLOCKED CLASS THAT PREVENTS DYNAMIC CREATION OF INSTANCES
+'''
 class LockedClass(object):
+    '''
+    WE USE SLOT
+    '''
     __slots__ = 'first_name'
