5a2b5a01e40aa94ba7e6429feebd193f6b08d133
SugiraVinc <sugiravincent11@gmail.com>
2022-07-29 04:09:38 -0700

    Is it an object of a class?

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' We import no modules'''
+
+
+def is_kind_of_class(obj, a_class):
+    """ function returns true for any existence of obj relative """
+    return isinstance(obj, a_class)
