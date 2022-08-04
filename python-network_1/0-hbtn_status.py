1769bde8bdd3d81171794dd60cbd8c52d4b7d39e
SugiraVinc <sugiravincent11@gmail.com>
2022-08-04 09:05:20 -0700

    our first url retrieval

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,4 +6,5 @@ import urllib.request
 '''We have to use with to fetch the url'''
 req = urllib.request.Request('https://intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
+    '''Printing the body of our fetch'''
     print(+str(req.getcode()))
