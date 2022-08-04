b58cb9071c29ecc266e67be3dbb26a8e33840c67
SugiraVinc <sugiravincent11@gmail.com>
2022-08-04 09:25:45 -0700

    our first url retrieval

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,8 @@
 import urllib.request
 
 
-'''We have to use with to fetch the url'''
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-'''Let us create a variable'''
-with urllib.request.urlopen(req) as response:
-    '''Printing the body of our fetch'''
-    print(+str(req.getcode()))
-'''We can now run it'''
+with urllib.request.urlopen('https://intranet.hbtn.io/status')
+html = response.read():
+    '''Let us display the content'''
+    print(html.content)
+
