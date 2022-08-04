546757742109ffba0b917b25eab1cb1e2f449d5c
SugiraVinc <sugiravincent11@gmail.com>
2022-08-04 09:13:12 -0700

    our first url retrieval

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' We are not importing any module other than urllib request'''
-
-
 import urllib.request
+
+
 '''We have to use with to fetch the url'''
 req = urllib.request.Request('https://intranet.hbtn.io/status')
 '''Let us create a variable'''
