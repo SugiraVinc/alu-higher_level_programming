53c65aab4f7690cff11e544e0fbd7dd046530b12
SugiraVinc <sugiravincent11@gmail.com>
2022-08-04 08:49:21 -0700

    our first url retrieval

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+''' We are not importing any module other than urllib request'''
+
+
 import urllib.request
+'''We have to use with to fetch the url'''
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
