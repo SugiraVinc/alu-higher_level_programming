06cdc3e511efa619bd2ee28c2be584840ff0db47
SugiraVinc <sugiravincent11@gmail.com>
2022-08-05 09:28:55 -0700

    raise the error

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -5,8 +5,6 @@ import sys
 
 if __name__ == '__main__':
     req = sys.argv[1]
-    data = sys.argv[2]
-    data = data.encode('utf-8')
     try:
         with urllib.request.urlopen(req) as e:
             print('Our request')
