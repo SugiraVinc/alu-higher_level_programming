193ffda81c3ced89b486bc436d7b54480ffacaf0
SugiraVinc <sugiravincent11@gmail.com>
2022-08-05 09:30:51 -0700

    raise the error

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     req = sys.argv[1]
     try:
         with urllib.request.urlopen(req) as e:
-            print('Our request')
+            print('Regular request')
     except urllib.error.HTTPError as e:
         print('Error code:', e.code)
