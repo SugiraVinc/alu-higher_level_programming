2ffb42b6923f062138e774a2e0fda800b9d3b0bb
SugiraVinc <sugiravincent11@gmail.com>
2022-08-16 09:59:00 -0700

    Our first unit test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ A function that adds two numbers """
 
-""" Our function that helps to add numbers"""
+
 def add_integer(a, b=98):
-    """
-    Raising the type error message
-    """
+    """ Raising the type error message"""
     if type(a) != int or float:
         raise TypeError(" a must be an integer ")
     if type(b) != int or float:
