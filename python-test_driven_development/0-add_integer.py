eb7e35141fba0feb61987a5b0b46c8f3c5c7fbd8
SugiraVinc <sugiravincent11@gmail.com>
2022-08-16 07:58:38 -0700

    our test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,6 +6,8 @@ def add_integer(a, b=98):
     try:
         if type(a) == int or float:
             sum = a+b
+        if type(b) == int or float:
+            sum = a+b
     except TypeError:
         print(" a must be an integer ")
     return sum
