7f9e07ab92afe5eb9ed71715dcc3d5b9db421657
SugiraVinc <sugiravincent11@gmail.com>
2022-08-16 10:19:26 -0700

    Our first unit test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,8 @@
 
 def add_integer(a, b=98):
     """ Raising the type error message"""
-    try:
-        if type(a) == int or float:
-            sum = a+b
-        if type(b) == int or float:
-            sum = a+b
-    except TypeError:
-        print(" a must be an integer ")
+        if type(a) != int or float:
+            raise TypeError("a must be an integer")
+        if type(b) != int or float:
+            raise TypeError("b must an integer")
     return int(a) + int(b)
