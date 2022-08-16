dde0e8a701d39ee435a360ffd69488405cb4f978
SugiraVinc <sugiravincent11@gmail.com>
2022-08-16 09:15:13 -0700

    Our test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,9 +6,9 @@ def add_integer(a, b=98):
     """
     Raising the type error message
     """
-    if not isinstance(a, int) or not isinstance(a, float):
+    if not isinstance(a) != int or float:
         raise TypeError(" a must be an integer ")
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b) != int or float:
         raise TypeError(" b must be an integer ")
     if isinstance(a, float):
         a = int(a)
