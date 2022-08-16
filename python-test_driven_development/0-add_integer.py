d0e751986aa88ad6a76c4f880fd3910f41a6f021
SugiraVinc <sugiravincent11@gmail.com>
2022-08-16 08:23:48 -0700

    our test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ def add_integer(a, b=98):
     """
     Raising the type error message
     """
-    if not type(a) == int or float:
+    if not type(a, int) or not type(a, float):
         raise TypeError(" a must be an integer ")
-    if not type(b) == int or float:
+    if not type(b, int) or not type(b, float):
         raise TypeError(" b must be an integer ")
     return a+b
