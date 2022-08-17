fac11c0171f1571274c5439224773047afecd20d
SugiraVinc <sugiravincent11@gmail.com>
2022-08-17 10:51:26 -0700

    Matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,7 +6,8 @@ def matrix_divided(matrix, div):
     """ A matrix function that will help us test """
     if type(matrix) != int or type(matrix) != float:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(matrix) != div:
+    for raw in matrix:
+        if len(matrix) != len(raw):
         raise TypeError("Each row of the matrix must have the same size")
     if type(div) != int or type(div) != float:
         raise TypeError("div must be a number")
