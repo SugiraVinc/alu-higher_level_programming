57a69405d11997e5243435d468103c776c2f110c
SugiraVinc <sugiravincent11@gmail.com>
2022-08-19 11:05:29 -0700

    display a name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""A module to print names"""
+"""
+A module to print names
+"""
 
 
 def def say_my_name(first_name, last_name=""):
-    """A function that prints out both my names"""
+    """
+    A function that prints out both my names
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
