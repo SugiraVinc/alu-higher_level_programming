5a148aa1a81b0c4bccd5539776c1581db0ceb3d3
SugiraVinc <sugiravincent11@gmail.com>
2022-08-20 07:28:39 -0700

    Spaces

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,14 +3,13 @@
 
 
 def text_indentation(text):
+    """Testing the function for idenation"""
     if type(text) != str:
         raise TypeError("text must be a string")
-    print(".", end="")
-    print("" + '\n')
-    print("" + '\n')
-    print("?", end="")
-    print("" + '\n')
-    print("" + '\n')
-    print(":")
-    print("" + '\n')
-    print("" + '\n')
+        line = ""
+    for c in text:
+        line += c
+        if c in ['.', '?', ':']:
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end="")
