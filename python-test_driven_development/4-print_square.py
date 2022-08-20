d52dc44ad19b6e1fb562df3c17a8d33a29a58e4b
SugiraVinc <sugiravincent11@gmail.com>
2022-08-20 06:57:57 -0700

    Square function

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) == float and size < 0:
+    if type(size) == float or size < 0:
         raise TypeError("size must be an integer")
     for i in size:
         print("#" * size)
