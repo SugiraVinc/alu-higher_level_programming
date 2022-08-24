92f920054e50ab4836cce1548bd16895e4d4c101
SugiraVinc <sugiravincent11@gmail.com>
2022-08-23 18:04:53 -0700

    rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A rectangular module"""
+from base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """class intance"""
+        Base.__init__(id)
+        self.width = width
+        self.height =height
+        self.x = x
+        self.y = y
