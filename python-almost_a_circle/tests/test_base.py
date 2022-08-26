37bb993434e7409b3f334ce86019582c980802ee
SugiraVinc <sugiravincent11@gmail.com>
2022-08-26 07:05:34 -0700

    None ID test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,7 +10,7 @@ from models.square import Square
 class TestBase_instantiation(unittest.TestCase):
     """Unittest instant"""
 
-    def test_None_ID(self):
+    def test_None_id(self):
         a = Base(None)
         b = Base(None)
-        self.assertEqual(a.id, b.id)
+        self.assertEqual(a.id, b.id - 1)
