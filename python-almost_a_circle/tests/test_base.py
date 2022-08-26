e420be6eb1204080f2ec2f4fca4cabfe3330ffd5
SugiraVinc <sugiravincent11@gmail.com>
2022-08-26 06:56:37 -0700

    None ID test

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for base.py"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittest instant"""
+
+    def test_None_ID(self):
+        a = Base(None)
+        b = Base(None)
+        self.assertEqual(a.id, b.id)
