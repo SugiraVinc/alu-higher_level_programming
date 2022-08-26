3426c9cf884f4650df9d92d76dff5675855d8b14
SugiraVinc <sugiravincent11@gmail.com>
2022-08-26 03:29:41 -0700

    SQL file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
     # My SQL is running on local  host an dat port 3306
-    crd = MySQLdb.connect(user=argv[1], passwd=argv[2], crd=argv[3])
-    call = crd.cursor()
+    dta = MySQLdb.connect(user=argv[1], passwd=argv[2], dta=argv[3])
+    call = dta.cursor()
     call.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     result = call.fetchall()
     for i in result:
         print(i)
     # close cursor and db
     call.close()
-    crd.close()
+    dta.close()
