a10cc06760b16753972bd254d037e306e23fdd0d
SugiraVinc <sugiravincent11@gmail.com>
2022-08-26 03:56:37 -0700

    Add more salt

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that retrieve database"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    # Usual SQL querry
+    cur.execute("select * FROM states WHERE states.name = '{}'\
+    ORDER BY states.id ASC".format(arg[4]))
+    result = cur.fetchall()
+    # Navigating through the arguments
+    for i in result:
+        if i[1] == argv[4]:
+            print(i)
+    # close cursor and db
+    cur.close()
+    db.close()
