5a6e87eecdd1f26124c05bd148a7d0d65b6c9268
SugiraVinc <sugiravincent11@gmail.com>
2022-08-26 04:12:01 -0700

    List tables based on ID

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to retrive data from table"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    # local host and port 3306
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    # usual SQL querry to link tables based on state_id
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC""")
+    result = cur.fetchall()
+    # Is name same as passed in the argument
+    for i in result:
+        print(i)
+    cur.close()
+    db.close()
